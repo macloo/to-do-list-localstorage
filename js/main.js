14c821db5760f4664a347e5573d4926752014fc7
macloo <mmcadams@gmail.com>
2017-01-29 12:15:59 -0500

    reads/writes perfectly
    Without option to remove items; no option to change order of items

--- js/main.js
+++ js/main.js
@@ -9,17 +9,23 @@ if ( storageAvailable('localStorage') ) {
         $('.lead').after('<p class="alert alert-success text-center">You' +
             ' don\'t have a To Do list on this device. Use the form to create' +
             ' your first item.</p>');
+        $('#paraToHideForm').hide();
+        $('#toDoDataRow').hide();
+
+    /* if toDoList does exist -- */
     } else {
-        // $('#newItemForm').hide();
-        // get localStorage string, parse into objects, put into array
-        myArray.push( JSON.parse( localStorage.getItem('maclooToDoList') ) );
+        $('#newItemForm').hide();
+        // get localStorage string, parse into objects, replace array
+        myArray = JSON.parse( localStorage.getItem('maclooToDoList') );
+        // write array contents to page
         writeOutToDoList();
     }
-
+/* if there is not localStorage */
 } else {
     $('.lead').after('<p class="alert alert-danger text-center">Oh, no!' +
     ' Your browser does not support local storage!</p>');
     $('#newItemForm').hide();
+    $('#toDoDataRow').hide();
 }
 
 /* test for existence of localStorage */
@@ -36,15 +42,65 @@ function storageAvailable(type) {
 	}
 }
 
-// listen for submit event
+// listen for form submit event
 $('#newItemForm').submit(function(e) {
     e.preventDefault();
     getFormData();
 });
 
-function writeOutToDoList(){
-    // myArray already has the objects
-    // stuff to write list - loop
+// listen for link click
+$('#hideForm').click(function(e) {
+    e.preventDefault();
+    $('#newItemForm').hide();
+    writeOutToDoList();
+});
+
+// listen for link click
+$('#showForm').click(function(e) {
+    e.preventDefault();
+    $('#newItemForm').show();
+    $('#toDoDataRow').hide();
+});
+
+function writeOutToDoList() {
+    var string = "";
+    for (var i = 0; i < myArray.length; i++) {
+        var itemname = myArray[i].name;
+        var descrip = myArray[i].descrip;
+        var duedate = myArray[i].date;
+        var designate = setPriority(myArray[i].priority);
+        var itemstring = '<div class="panel panel-default">' +
+            '<div class="panel-body">' + '<span class="label label-' +
+             designate[0] + '">' + designate[1] + '</span> ' +
+            itemname + ' &mdash; ' + descrip +
+            '</div><div class="panel-footer">DUE: ' +
+            duedate + '</div></div>';
+        var remove = '';
+        string += itemstring;
+    }
+    $('#toDoDataRow').show();
+    $('#toDoData').html("").append(string);
+}
+
+function setPriority(priority) {
+    var designate, level;
+    if (priority == 1) {
+        designate = "danger";
+        level = "Highest";
+    } else if (priority == 2) {
+        designate = "warning";
+        level = "High";
+    } else if (priority == 3) {
+        designate = "success";
+        level = "Medium";
+    } else if (priority == 4) {
+        designate = "info";
+        level = "Low";
+    } else {
+        designate = "primary";
+        level = "Lowest";
+    }
+    return [designate, level];
 }
 
 function getFormData() {
@@ -60,23 +116,22 @@ function getFormData() {
     $('#descrip').val("");
     $('#priority').val("");
 
-    console.log(newItem + " " + newDescrip + " " + newDate + " " + newPriority);
-
     // assign object properties
     toDoItem.name = newItem;
     toDoItem.descrip = newDescrip;
     toDoItem.date = newDate;
     toDoItem.priority = newPriority;
 
-    // add new item to the array
+    // add the new item to the array
     myArray.push(toDoItem);
 
     // write it out to localStorage right away
     localStorage.setItem( 'maclooToDoList', JSON.stringify(myArray) );
 
-    // all objects are in the array: myArray is completeList
-    // localStorage.setItem(x, x); // name, value
-    // localStorage.setItem( 'maclooToDoList', JSON.stringify(completeList) );
+    // after writing, hide form and show all items
+    $('#toDoDataRow').show();
+    writeOutToDoList();
+    $('#newItemForm').hide();
 }
 
 }); // end document ready
